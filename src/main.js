313c9573c668cca82154e8b0aa202eb29d6f5df0
Lamontjas <jlamont@stu.norwich.edu>
2016-06-29 17:06:04 -0400

    initial

---
+++ src/main.js
@@ -0,0 +1,57 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require ('role.upgrader');
+var roleBuilder = require ('role.builder');
+module.exports.loop = function () {
+   //clear memory
+
+    for (let names in Memory.creeps)
+    {
+        if (Game.creeps[names]==undefined)
+        {
+            delete Memory.creeps[names];
+        }
+    }
+
+    for(var name in Game.creeps)
+    {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role =='upgrader')
+        {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role =='builder')
+        {
+            roleBuilder.run(creep);
+        }
+
+    }
+
+    var newCreep = Game.rooms.E38N3.lookForAtArea(LOOK_CREEPS,43,37,45,39,true);
+    console.log(newCreep.length);
+
+
+
+    var minimumHarvesters = 3;
+    var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role =='harvester');
+    var numberOfUpgraders= _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+    var numberOfBuilders= _.sum(Game.creeps, (c) => c.memory.role == 'builder');
+
+    if (numberOfUpgraders<3)
+    {
+        var newUpgrader = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE,MOVE],undefined,{role:'upgrader',upgrading: false})
+    }
+    if (numberOfBuilders<3)
+    {
+        var newUpgrader = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE,MOVE],undefined,{role:'builder',building:false})
+    }
+
+    if((numberOfHarvesters<minimumHarvesters))
+    {
+       var name =  Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE],undefined,{role:'harvester'})
+   }
+
+}
+
---
+++ src/role.upgrader.js
@@ -0,0 +1,57 @@
+var roleUpgrader = {
+
+    run: function (creep)
+    {
+        var newCreep = Game.rooms.E38N3.lookForAtArea(LOOK_CREEPS,43,37,45,39,true);
+
+       if (creep.memory.upgrading==true && creep.carry.energy==0)
+       {
+           creep.memory.upgrading = false;
+           if (creep.memory.source!=0||creep.memory.source!=1)
+           {
+               if (newCreep.length>4)
+               {
+                   creep.memory.source=1;
+               }
+               else
+               {
+                   creep.memory.source=0;
+               }
+           }
+
+       }
+        if (creep.memory.upgrading ==false && creep.carry.energy == creep.carryCapacity)
+        {
+            creep.memory.upgrading = true;
+            creep.memory.source=-1;
+        }
+
+        if(creep.memory.upgrading){
+            if(creep.upgradeController(creep.room.controller)==ERR_NOT_IN_RANGE)
+            {
+                creep.moveTo(creep.room.controller);
+
+            }
+
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+
+
+            if (creep.memory.source==0) {
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE ) {
+                    creep.moveTo(sources[0]);
+                }
+            }
+            else if (creep.memory.source==1)
+            {
+                if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE ) {
+                    creep.moveTo(sources[1]);
+                }
+            }
+
+        }
+    }
+
+};
+module.exports = roleUpgrader;
